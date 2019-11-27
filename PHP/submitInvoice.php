6a4ced38891c19fdc2734dddae50f96a3687b69f
Cam Lloyd <31662072+CLloyd728@users.noreply.github.com>
2019-11-26 19:30:34 -0600

    Create submitInvoice.php

---
+++ PHP/submitInvoice.php
@@ -0,0 +1,12 @@
+<?php
+ try { //exception thrown if something happens
+                $dsn = "mysql:host=courses;dbname=z1853137";
+                $pdo = new PDO($dsn, "z1853137", "1998Mar21");
+        } catch (PDOexception $e) { //catch the exception
+                echo "Connection to DB failed: " . $e->getMessage();
+	}
+	$subInv = "UPDATE orderHistory SET status='shipped' WHERE orderID=?;";
+	$subInvResult = $pdo->prepare($subInv);
+	$subInvResult->execute(array($_POST['OID']));
+header("Refresh:5; url=/~z1853137/467admin.php");
+?>
